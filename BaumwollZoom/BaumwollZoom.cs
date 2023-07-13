faf36ef24d342e36fa569c49c3b4cd1a438a1253
Michael Binder <rednib.coding@gmail.com>
2023-07-13 23:30:35 +0200

    Initial commit

---
+++ BaumwollZoom/BaumwollZoom.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ICities;
+using UnityEngine;
+
+namespace BaumwollZoom
+{
+    public class BaumwollZoomMod : IUserMod
+    {
+        public string Name
+        {
+            get { return "BaumwollZoom Zoom to Cursor"; }
+        }
+
+        public string Description
+        {
+            get { return "Modifies the camera behaviour such that scrolling zooms in or out while keeping the cursor in the same place."; }
+        }
+    }
+
+
+    public class BaumwollZoomLoader : LoadingExtensionBase
+    {
+        BaumwollZoomBehaviour instance;
+
+        public override void OnLevelUnloading()
+        {
+            if (instance != null)
+            {
+                MonoBehaviour.Destroy(instance);
+            }
+            base.OnLevelUnloading();
+        }
+
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            instance = GameObject.FindObjectOfType<CameraController>().gameObject.AddComponent<BaumwollZoomBehaviour>();
+            base.OnLevelLoaded(mode);
+        }
+    }
+
+
+    public class BaumwollZoomBehaviour : MonoBehaviour
+    {
+        private CameraController cameraController;
+
+        private static RayCaster raycaster = new RayCaster();
+
+        private void Start()
+        {
+            cameraController = GameObject.FindObjectOfType<CameraController>();
+            cameraController.m_maxTiltDistance = 1000000f;
+        }
+
+        private float frameInitialCurrentSize;
+
+        private RayCaster.RaycastInput frameInitialMouseRaycastInput;
+
+        private void Update()
+        {
+            frameInitialCurrentSize = cameraController.m_currentSize;
+            frameInitialMouseRaycastInput = new RayCaster.RaycastInput(
+                Camera.main.ScreenPointToRay(Input.mousePosition),
+                Camera.main.farClipPlane
+            );
+        }
+
+        private void LateUpdate()
+        {
+            if (frameInitialCurrentSize == 0f || cameraController.m_currentSize == 0f || frameInitialCurrentSize == cameraController.m_currentSize)
+            {
+                return;
+            }
+            RayCaster.RaycastOutput output;
+            if (raycaster.CastRay(frameInitialMouseRaycastInput, out output))
+            {
+                var fractionTowardsCursor = 1f - cameraController.m_currentSize / frameInitialCurrentSize;
+                var correction = fractionTowardsCursor * (output.m_hitPos - cameraController.m_currentPosition);
+                correction.y = 0f;
+                cameraController.transform.position += correction;
+                cameraController.m_targetPosition += correction;
+                cameraController.m_currentPosition += correction;
+            }
+        }
+    }
+
+    class RayCaster : ToolBase
+    {
+        public bool CastRay(RaycastInput input, out RaycastOutput output)
+        {
+            return RayCast(input, out output);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # BaumwollZoom
+
 Zoom to cursor mod for Cities: Skylines without any dependencies
+
+# How to install the mod localy
+
+Once you have created and compiled the mod, Visual Studio generates a .dll file. This file contains the code of the mod and needs to be placed in the correct folder so that Cities: Skylines can load it.
+
+The path to the mods folder in Cities: Skylines depends on your operating system. On Windows, you can typically find it here:
+
+C:\Users\[Your Username]\AppData\Local\Colossal Order\Cities_Skylines\Addons\Mods\[Name of the Mod]\
+
+Replace [Your Username] with your actual username and [Name of the Mod] with the name of your mod. This folder should contain the .dll file of the mod.
+
+For example, if your username is user and you have named the mod ZoomToCursorMod, the path should look like this:
+
+C:\Users\user\AppData\Local\Colossal Order\Cities_Skylines\Addons\Mods\ZoomToCursorMod\
+
+Copy the .dll file into this folder. The next time you start Cities: Skylines, you should see the mod in the game's mod menu. Remember to activate it for the changes to take effect.
+
+Note: The AppData folder is typically hidden, so you may need to show hidden folders to access it. You can also type %LocalAppData% into the address bar of the File Explorer to directly go to the AppData\Local folder.
